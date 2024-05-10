020d7806a6ce095a7318c3b7bb82ce5e8b46e35b
roksana35 <roksana7235@gmail.com>
2024-05-10 12:28:40 +0600

    add firebase condig file

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,6 +8,12 @@ const Register = () => {
         <form className="bg-black p-8 bg-opacity-45 rounded-lg shadow-lg m-5 w-2/4">
    <div className="form-control">
      <label className="label">
+       <span className="label-text text-white">Name</span>
+     </label>
+     <input type="text" placeholder="Name" className="input input-bordered w-full" required />
+   </div>
+   <div className="form-control">
+     <label className="label">
        <span className="label-text text-white">Email</span>
      </label>
      <input type="email" placeholder="email" className="input input-bordered w-full" required />
@@ -19,15 +25,19 @@ const Register = () => {
      <input type="password" placeholder="password" className="input input-bordered    w-full" required />
      
    </div>
+   <div className="form-control">
+     <label className="label">
+       <span className="label-text text-white">PhotoURL</span>
+     </label>
+     <input type="text" placeholder="PhotoURL" className="input input-bordered    w-full" required />
+     
+   </div>
    <div className="form-control mt-6">
-     <button className="btn btn-primary w-full">Login</button>
+     <button className="btn btn-primary w-full">Register</button>
    </div>
    <div className="text-center mt-3 ">
    <span className="text-white text-center"> Already have an account then <Link to='/login'><button className="text-white font-semibold text-purple-600 ml-3">Login</button></Link></span> 
-   <div className=" p-2 lg:ml-7 mx-auto">
- <button  className="btn btn-outline border-2 text-white border-purple-800 mx-auto"> Continue with Google</button>
-</div>
-
+   
    </div>
   
 
